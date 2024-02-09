540932af62b7b612d24c9548f4dfa44133ff3df5
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2024-02-09 13:10:50 +0530

    host chng

--- MedML/main.py
+++ MedML/main.py
@@ -48,4 +48,4 @@ async def create_upload_files(audiofile: UploadFile):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 8001))
-    run(app, host="127.0.0.1", port=port)
+    run(app, host="0.0.0.0", port=port)
