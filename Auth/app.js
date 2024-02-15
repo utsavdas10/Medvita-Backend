225dcd9593b88746b5e504a73c1e0e3b7e6753d8
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2024-02-15 11:26:52 +0530

    ADDED PATCH

--- Auth/app.js
+++ Auth/app.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
         'Access-Control-Allow-Headers',
         'Origin, X-Requested-With, Content-Type, Accept, Authorization'
     ); // Allow these headers
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST'); // Allow these methods
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE'); // Allow these methods
     next();
 });
 
