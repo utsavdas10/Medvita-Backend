37d6a68b999a8fcc580e33c20505ae911e9cfadb
utsavdas10 <97392403+utsavdas10@users.noreply.github.com>
2024-02-19 15:48:24 +0530

    yoga videos

--- Community/models/users-model.js
+++ Community/models/users-model.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    posts : [{type: mongoose.Types.ObjectId, required: true, ref: 'Post'}],
+    posts : [{type: mongoose.Types.ObjectId, required: true, ref: 'Post'}]
 });
 
 module.exports = mongoose.model('User', userSchema);
